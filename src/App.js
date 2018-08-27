0ed9eb8a278ec8532b060ec7142f2d6a88a22859
lolita89 <cika.lolita@hotmail.com>
2018-08-27 22:27:01 +0200

    putting counter app and playing arround with sass

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './app.scss';
+import Counter from './components/Counter';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         test
+        <Counter />
       </div>
     );
   }
